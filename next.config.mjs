228cf58a1b443b13ac996b21e9378677d3f93730
Bharath <bharathh.868@gmail.com>
2026-03-06 00:39:58 -0800

    fix: NODE_ENV production issu fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 const repoName = "mcp-servers"; // change this to match your GitHub repo name
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig = {
   reactCompiler: true,
@@ -8,8 +8,8 @@ const nextConfig = {
   // When hosted at https://username.github.io/mcp-servers/
   // set basePath and assetPrefix to the repo name.
   // Remove these two lines if using a custom domain or deploying to the root.
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
+  basePath: isGithubActions ? `/${repoName}` : "",
+  assetPrefix: isGithubActions ? `/${repoName}/` : "",
   images: {
     unoptimized: true, // required for static export
   },
